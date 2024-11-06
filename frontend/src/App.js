ba4d5004e4c6de6c0d5161e2d3c1f436dc6702c5
hamadismail <hamad.ismail.gub@gmail.com>
2024-11-06 16:34:18 +0600

    add-header-footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
-      <h1>E Shop</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to e-Shop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
